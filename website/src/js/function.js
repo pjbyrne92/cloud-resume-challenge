0fb52cafdb42a953c53c908bb0a0c546aaca2c0d
PJ Byrne <pjbyrne92@gmail.com>
2023-05-01 11:43:23 +0300

    Update function.js

--- website/src/js/function.js
+++ website/src/js/function.js
@@ -72,7 +72,7 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch(
-        "https://wwjcx7tyxrbjmbkf3vc3teo3mu0qrvhq.lambda-url.ca-central-1.on.aws/"
+        "https://tz7epthvo4g3nqg6hv4elo2p2i0nuoar.lambda-url.eu-west-1.on.aws/"
     );
     let data = await response.json();
     counter.innerHTML = `👀 Views: ${data}`;
